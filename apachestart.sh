e750e2f0a7dcea9788e2d9fe699cdbfdf4bff001
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2023-02-01 11:17:03 -0500

    configure global hostname on launch

--- apachestart.sh
+++ apachestart.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eux
 
+# Generate self-signed cert
 dpkg-reconfigure ssl-cert
+# Configure global default hostname
+echo "ServerName $(hostname)" >> /etc/apache2/apache2.conf
+# Validate config
 apachectl configtest
+# Launch Apache
 apachectl -DFOREGROUND
